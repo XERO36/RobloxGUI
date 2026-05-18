a275e53f0233b7df50fb4c73bb2ca55fafcda48a
Dev1994eke <Devime7289128912@outlook.com>
2026-05-18 10:17:30 +0200

    Update pianoautoplayer.lua - bug fixes

--- Games/pianoautoplayer.lua
+++ Games/pianoautoplayer.lua
@@ -1,2 +1,2 @@
 local typeloader = loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/refs/heads/main/ScriptPart/Type.lua"))()
-local nokick = loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/refs/heads/main/ScriptPart/anti-kick.lua")
+local nokick = loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/refs/heads/main/ScriptPart/anti-kick.lua"))()
