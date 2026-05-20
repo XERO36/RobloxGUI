04d75917b76c0e553c3e637d7b83c656a1683811
Dev1994eke <Devime7289128912@outlook.com>
2026-05-20 11:28:45 +0200

    Update 99nights.lua

--- Games/99nights.lua
+++ Games/99nights.lua
@@ -9,4 +9,32 @@ local tobring = {"Carrot", "Berry", "Chilli", "Apple", "Morsel", "Steak"}
 local enemys = workspace:FindFirstChild("Characters") -- but check each item inside the folder without getting the players body yk what I mean
 local feedFire = false
 local lookingforpl = false
+-- Example layout setup if loading inline
+local imgui = loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/refs/heads/main/CustomImGUI/source.lua"))()
 
+local window = imgui:createWindow({
+    Main = {
+        Name = "99 Nights in The Forest",
+        Creator = "ShadowDev"
+    }
+})
+local mainTab = window:createTab("Main")
+local visualTab = window:createTab("Visual")
+local MFunc = mainTab:AddLabel("Main functions")
+local autoFire = mainTab:AddToggle({
+    Name = "Auto Fire",
+    Default = false,
+    Callback = function(state)
+        -- auto fire function 
+    end
+})
+for _, in ipairs(Scraps) do 
+  local killBtn = mainTab:AddButton({
+    Name = Scraps,
+    Callback = function()
+        -- bring
+    end
+})
+end
+
+local MValuer = mainTab:AddLabel("Main Values for the Main functions")
