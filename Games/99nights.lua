e5d2fa5e25a4bb570d10683196a80aca6f0fcd5d
Dev1994eke <Devime7289128912@outlook.com>
2026-05-20 11:24:20 +0200

    Update 99nights.lua

--- Games/99nights.lua
+++ Games/99nights.lua
@@ -1,1 +1,12 @@
-local w = workspace
+local heal = {"MedKit", "Bandage"}
+local fuel = {"Fuel Canister", "Coal", "Log", "Bio Fuel", "Oil Barrel", "Cultist", "Crossbow Cultist", "Wolf Corpse", "Alpha Wolf Corpse", "Bear Corpse"}
+local Scraps = {"Old Radio", "Sheet Metal", "Bolt", "UFO Junk", "UFO Component", "Broken Fan", "Broken Microwave", "Tyre", "Metal Chair", "Old Car Engine", "Washing Machine", "Cultist Experiment", "Cultist Prototype", "UFO Scrap"}
+local Ammo = {"Revolver Ammo", "Rifle Ammo", "Shotgun Ammo"}
+local Armor = {"Leather Body", "Iron Body", "Thorn Body", "Riot Shield", "Alien Armor"}
+local weapons = {"Spear", "Morningstar", "Laser Sword", "Ice Sword", "Revolver", "Rifle", "Shotgun"}
+local eatable = {"Carrot", "Berry", "Chilli", "Apple", "Cooked Morsel", "Cooked Steak"}
+local tobring = {"Carrot", "Berry", "Chilli", "Apple", "Morsel", "Steak"}
+local enemys = workspace:FindFirstChild("Characters") -- but check each item inside the folder without getting the players body yk what I mean
+local feedFire = false
+local lookingforpl = false
+
