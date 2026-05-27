1ce10840eca9d4541b6ea56191041122e375d525
SDev <Devime7289128912@outlook.com>
2026-05-27 11:18:56 +0200

    Update main.lua

--- Exec/main.lua
+++ Exec/main.lua
@@ -4,4 +4,12 @@ local p = {}
 
 function clhud.add(n)
   if n == "main.hud" then
-    local sgui = Instance.new
+    
+    local c8ot92 = Instance.new("ScreenGui",game.CoreGUI)
+    local sgui = c8ot92
+
+  end
+end
+
+return clhud
+      
