22c709d8e457e3ec480fb60a6e9a3e1204ba1b9a
SDev <Devime7289128912@outlook.com>
2026-05-27 12:13:14 +0200

    Update main.lua

--- Exec/main.lua
+++ Exec/main.lua
@@ -4,7 +4,20 @@ local p = {}
 
 function clhud.add(n)
   if n == "main.hud" then
-    
+
+    local function getX()
+	local camera = workspace.CurrentCamera
+	local viewportSize = camera.ViewportSize
+
+      return viewportSize.X
+end
+    local function getY()
+	local camera = workspace.CurrentCamera
+	local viewportSize = camera.ViewportSize
+
+      return viewportSize.Y
+end
+
     local c8ot92 = Instance.new("ScreenGui",game.CoreGUI)
     local sgui = c8ot92
 
