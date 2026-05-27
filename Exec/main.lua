2080410e00d055bbeaa1a5152964b283c0f39ac1
SDev <Devime7289128912@outlook.com>
2026-05-27 12:21:17 +0200

    Update main.lua

--- Exec/main.lua
+++ Exec/main.lua
@@ -4,7 +4,7 @@ local p = {}
 
 function clhud.add(n)
   if n == "main.hud" then
-
+		local device 
     local function getX()
 	local camera = workspace.CurrentCamera
 	local viewportSize = camera.ViewportSize
@@ -18,18 +18,18 @@ end
       return viewportSize.Y
 end
 		if getX() <=768 and getY() <= 1024 then
-	print("Phone")
+	device = "Phone"
 elseif getX() <= 1024 and getY() <= 1366 then
-	print("Tablet")
+	device = "Tablet"
 else
-	print("PC")
+	device = "PC"
 end
     local c8ot92 = Instance.new("ScreenGui",game.CoreGUI)
     local sgui = c8ot92
 
     local main = Instance.new("Frame",sgui)
     local maincorner =  Instance.new("UICorner",main)
-    local device 
+    
 
     
   end
