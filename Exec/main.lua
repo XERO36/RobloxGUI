8f61c443ad03b1bdeda6edd65dfdd11862d1ce0d
SDev <Devime7289128912@outlook.com>
2026-05-28 10:49:46 +0200

    Update main.lua

--- Exec/main.lua
+++ Exec/main.lua
@@ -4,37 +4,10 @@ local p = {}
 
 function clhud.add(n)
   if n == "main.hud" then
-		local device 
-    local function getX()
-	local camera = workspace.CurrentCamera
-	local viewportSize = camera.ViewportSize
-
-      return viewportSize.X
-end
-    local function getY()
-	local camera = workspace.CurrentCamera
-	local viewportSize = camera.ViewportSize
-
-      return viewportSize.Y
-end
-		if getX() <=768 and getY() <= 1024 then
-	device = "Phone"
-elseif getX() <= 1024 and getY() <= 1366 then
-	device = "Tablet"
-else
-	device = "PC"
-end
-    
 		
-		if device == "Phone" then 
+
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/main/Exec/devicemobile.lua"))()
-		elseif device == "Tablet" then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/main/Exec/devicetablet.lua"))()
-		elseif device == "PC" then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/XERO36/RobloxGUI/main/Exec/devicepc.lua"))()
-		else
-			error("ERROR")
-		end
+		
   end
 end
 
