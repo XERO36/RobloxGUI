3b4babae192df075db31629002259712fea46a99
SDev <Devime7289128912@outlook.com>
2026-08-10 11:18:55 +0200

    Create duper.lua for ImGui integration
    Added duper.lua with initial setup for ImGui.

---
+++ Games/duper.lua
@@ -0,0 +1,8 @@
+local backspacekey = Enum.KeyCode.Backspace
+local hasequippeditem
+
+-- Example layout setup if loading inline
+local imgui = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/XERO36/RobloxGUI/refs/heads/main/CustomImGUI/source.lua"
+  ))()
+
